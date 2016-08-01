0ef23ebc3727c1ab8c9f47a259dbceb9aa5f00a6
zhangjinxing <zhangjinxing@chinazjgt.com>
2016-08-01 18:12:35 +0800

    init

---
+++ src/main/java/cn/test/services/service/HelloImpl.java
@@ -0,0 +1,11 @@
+package cn.test.services.service;
+
+import cn.test.interfaces.Hello;
+
+public class HelloImpl implements Hello{
+
+	public void sayHello() {
+      System.out.println("say Hello");		
+	}
+
+}
